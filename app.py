d9d33dd93330d1cf07c62fbb46b944f6130b796c
itxxahsan123 <jacklewis0084@gmail.com>
2024-09-23 20:47:43 +0500

    my 1st commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World! This is my CodePipeline Project and Successfully Running and Fucking Running........!!! I\'m host %s' % socket.gethostname()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
